ef067a7fa3aac5c9e34b74f15448c85a708b7e37
Eduardo Henrique <henriquebjj.uf@gmail.com>
2023-06-14 23:44:49 -0300

    Add files via upload

--- Projeto-NPL_Burpee/script.js
+++ Projeto-NPL_Burpee/script.js
@@ -2,6 +2,8 @@
 const userInput = document.getElementById('userInput');
 const chatlog = document.querySelector('.chatlog');
 
+
+
 // Evento de tecla pressionada
 userInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
@@ -39,7 +41,35 @@ function sendMessage() {
 
 // Função para processar a mensagem e obter a resposta do chatbot
 function processMessage(message) {
+    // Cria um objeto com a pergunta do usuário
+    const questionObj = {
+        question: message
+    };
 
-    return 'Ola, eu sou o Burpee, como posso le ajudar?';
+    // Faz a requisição para a API
+    fetch('https://3549-34-83-192-194.ngrok.io/api/answer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(questionObj)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Obtém a resposta do chatbot do objeto de resposta
+        const botResponse = data.answer;
 
+        // Cria um elemento de parágrafo para exibir a resposta do chatbot
+        const botMessage = document.createElement('p');
+        botMessage.classList.add('bot-message');
+        botMessage.textContent = botResponse;
+
+        // Adiciona a resposta do chatbot ao chatlog
+        chatlog.appendChild(botMessage);
+    })
+    .catch(error => {
+        console.error('Erro na requisição:', error);    
+    });
+
+    return 'Aguarde...'; // Mensagem temporária enquanto a resposta é obtida do servidor
 }
